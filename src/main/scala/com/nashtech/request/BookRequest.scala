20055b1ccf4b10a6b130c81c601a810ac991506d
Manish Mishra <manish.mishra@knoldus.com>
2023-05-19 02:20:48 +0530

    Added book request to perform http method

---
+++ src/main/scala/com/nashtech/request/BookRequest.scala
@@ -0,0 +1,90 @@
+package com.nashtech.request
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import com.nashtech.implement.book.BookImplementation
+import com.nashtech.models.Book
+import com.nashtech.models.BookJson.bookFormat
+import com.nashtech.validator.BookValidator
+import play.api.libs.json.Json
+
+object BookRequest extends App {
+  implicit val parseJson = Json.format[Book]
+  private val host = "localhost"
+  private val port = 8090
+  implicit val system = ActorSystem("HTTP_SERVER")
+  implicit val ec = system.dispatcher
+  val bookImplementation = new BookImplementation(new BookValidator)
+  val listOfBook = List(
+    Json.parse(
+      """
+        |{
+        | "id" : 202301,
+        | "title" : "Scala Basics",
+        | "price" : 200,
+        | "isbn" : 121416,
+        | "authorId" : 101
+        | }
+        |""".stripMargin
+    ).as[Book],
+    Json.parse(
+      """
+        |{
+        | "id" : 202302,
+        | "title" : "Scala Intermediate",
+        | "price" : 400,
+        | "isbn" : 121190,
+        | "authorId" : 102
+        | }
+        |""".stripMargin
+    ).as[Book]
+  )
+  listOfBook.map(book => bookImplementation.create(book))
+  val routes: Route = {
+    pathPrefix("api") {
+      path("books") {
+        post {
+          entity(as[Book]) { book =>
+            bookImplementation.create(book)
+            complete("Created Book")
+          }
+        } ~
+          get {
+            val success = bookImplementation.getAll()
+            success match {
+              case Right(value) => complete(StatusCodes.OK, s"${value.map(book => Json.toJson(book))}")
+              case Left(value) => complete(StatusCodes.NotFound, s"$value")
+            }
+          }
+      } ~
+        path("book" / IntNumber) { bookId =>
+          get {
+            val success = bookImplementation.get(bookId)
+            success match {
+              case Some(value) => complete(StatusCodes.OK, s"${Json.toJson(value)}")
+              case None => complete(StatusCodes.NotFound)
+            }
+          }
+        } ~
+        path("create-book" / IntNumber) { authorId =>
+          put {
+            entity(as[Book]) { book =>
+              println(bookImplementation.put(book.copy(authorId = authorId)))
+              bookImplementation.put(book.copy(authorId = authorId))
+              complete(StatusCodes.OK, s"${bookImplementation.put(book.copy(authorId = authorId))}")
+            }
+          }
+        }
+    }
+  }
+  val bindingFuture = Http().newServerAt(host, port).bind(routes)
+  println("Server available @ : http://localhost:8090/api")
+  sys.addShutdownHook {
+    bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
+  }
+}
+
